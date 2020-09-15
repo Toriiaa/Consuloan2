57d1d69d93931415bdd1c582152d5b7252ee04e1
Toriiaa <59958826+Toriiaa@users.noreply.github.com>
2020-09-15 18:43:14 +0100

    Add project files.

---
+++ Consuloan2/Content/js/consl.js
@@ -0,0 +1,12 @@
+ document.querySelector("#button").addEventListener('submit', function(e){
+    Hello();
+
+
+    e.preventDefault(),
+});
+Hello(){
+    alert:{"hi this is a string"};
+}
+
+
+
---
+++ Consuloan2/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Consuloan2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Title = "My ABOUT PAGE";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Title = "contact form";
+
+            return View();
+        }
+        public ActionResult Services()
+        {
+            ViewBag.Title = "Your services page.";
+
+            return View();
+        }
+    }
+}
